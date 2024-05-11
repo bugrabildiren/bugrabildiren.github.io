ca74674db3a6b63789bfade0a5db3038f10acdd1
bugrabildiren <121932578+bugrabildiren@users.noreply.github.com>
2024-05-11 22:22:14 +0300

    Update faq.js

--- UstBilTourismProject/faq.js
+++ UstBilTourismProject/faq.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
-    $("#accordion").accordion({
+    $(".accordion").accordion({
         heightStyle: "content",
         collapsible: true,
         active: false
     });
 });
+
+    var acc = document.getElementsByClassName("accordion");
+          var i;
+    
+          for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function () {
+              this.classList.toggle("active");
+              this.parentElement.classList.toggle("active");
+    
+              var pannel = this.nextElementSibling;
+    
+              if (pannel.style.display === "block") {
+                pannel.style.display = "none";
+              } else {
+                pannel.style.display = "block";
+              }
+            });
+          }
