d3cce115579568debfafafe7a33cc168c60e01fe
bugrabildiren <bugrabildiren@gmail.com>
2024-05-24 23:47:02 +0300

    güncel gelecek

--- UstBilTourismProject/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("#policies").click(function() {
-        $("#dialog").dialog();
-    });
-
-    $("#dialog").on("dialogclose", function(event, ui) {
-        $(this).dialog("destroy").html("");
-    });
-});
