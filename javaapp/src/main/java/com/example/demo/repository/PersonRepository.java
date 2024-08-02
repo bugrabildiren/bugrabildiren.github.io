0bc8e416ae2977233238dc9d3b41b8baaccb2b76
bugrabildiren <121932578+bugrabildiren@users.noreply.github.com>
2024-08-02 12:38:27 +0300

    Delete javaapp directory

--- javaapp/src/main/java/com/example/demo/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entitiy.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-}
